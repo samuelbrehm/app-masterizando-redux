269cf5b3d663c7cc462d5d33b7f050987288fd11
samuelbrehm <samuelbrehm@gmail.com>
2019-03-19 14:50:08 -0300

    Configurando primeiras actions

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,16 +1,20 @@
 import React from 'react';
 
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
 
 import { connect } from 'react-redux';
 
 // import styles from './styles';
 
-const TodoList = ({ todos }) => (
+const TodoList = ({ todos, dispatch }) => (
   <View style={{ flex: 1, backgroundColor: '#FFF', justifyContent: 'center' }}>
     {todos.map(todo => (
       <Text key={todo}>{todo}</Text>
     ))}
+    <Button
+      onPress={() => dispatch({ type: 'ADD_TODO', text: 'Novo todo' })}
+      title="Adicionar todo"
+    />
   </View>
 );
 
