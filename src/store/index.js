0f0395ae3c9d9a9273b46bcd7e60bc79471d4b61
samuelbrehm <samuelbrehm@gmail.com>
2019-03-20 11:44:46 -0300

    Criado a combinação de reducers

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,11 @@
 import { createStore, compose } from 'redux';
 
-import reducer from './reducers/login';
+import reducer from './reducers';
 
 const store = __DEV__
   ? createStore(reducer, compose(console.tron.createEnhancer()))
   : createStore(reducer);
+
+console.tron.log(store.getState());
 
 export default store;
