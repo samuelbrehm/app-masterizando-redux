df5bf446ce5dd086719c1a3c48ecc3f36b9f2c53
samuelbrehm <samuelbrehm@gmail.com>
2019-03-21 15:08:36 -0300

    Código refatorado para o pattern duck

--- src/pages/Repositories/index.js
+++ src/pages/Repositories/index.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import * as RepositoriesActions from '~/store/actions/repositories';
+import { Creators as RepositoriesActions } from '~/store/ducks/repositories';
 
 import { ActivityIndicator, Text } from 'react-native';
 
